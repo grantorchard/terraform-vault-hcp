303d8e2d9b1db06502d76974de7265df0dfc828c
Grant Orchard <go@hashicorp.com>
2022-03-08 13:18:48 +1100

    update jwt auth for github action

--- auth_jwt.tf
+++ auth_jwt.tf
@@ -11,10 +11,12 @@ resource "vault_jwt_auth_backend_role" "example" {
 		"default",
 		"kv_read"
 	]
-
+	verbose_oidc_logging = true
+	#bound_subject = "repo:grantorchard/gha-vault-jwt"
   bound_audiences = ["sigstore"]
   bound_claims = {
     "repository" = "grantorchard/gha-vault-jwt"
+		"ref"        = "refs/heads/main"
   }
   user_claim      = "actor"
   role_type       = "jwt"
